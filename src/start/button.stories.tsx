5d17c8866ad344baa581558fcda4ca5f9a26b2a6
Don Hsieh <don.shieh@gmail.com>
2019-05-06 23:34:38 +0800

    chore: Add tslint, prettier

--- src/start/button.stories.tsx
+++ src/start/button.stories.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
 import { storiesOf } from "@storybook/react";
+import * as React from "react";
 import Button from "./button";
 
 storiesOf("Button", module)
--- src/start/button.tsx
+++ src/start/button.tsx
@@ -1,24 +1,24 @@
-import * as React from "react";
+import * as React from 'react'
 
-type Props = {
-  children: React.ReactNode;
-  disabled?: boolean;
-  onClick?: (e: React.SyntheticEvent<HTMLButtonElement>) => any;
-};
+interface IProps {
+  children: React.ReactNode
+  disabled?: boolean
+  onClick?: (e: React.SyntheticEvent<HTMLButtonElement>) => any
+}
 
-const Button: React.ComponentType<Props> = ({
+const Button: React.ComponentType<IProps> = ({
   children,
   onClick,
-  disabled
+  disabled,
 }) => (
   <button disabled={disabled} onClick={onClick}>
     {children}
   </button>
-);
+)
 
 Button.defaultProps = {
   disabled: false,
-  onClick: () => {}
-};
+  onClick: () => {},
+}
 
-export default Button;
+export default Button
