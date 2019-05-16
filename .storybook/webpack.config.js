8057f284f6c44b2b51a56c06bad15bbcee0c406e
Don Hsieh <don.shieh@gmail.com>
2019-05-17 00:15:52 +0800

    feat: Add mdx example

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,16 +1,63 @@
+const fs = require('fs');
+const R = require('ramda');
+
 module.exports = ({ config }) => {
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    use: [
-      {
-        loader: require.resolve("awesome-typescript-loader")
+  const mdxRule = R.pipe(
+    R.find(r => `${r.test}` === '/\\.(mjs|jsx?)$/'),
+    R.evolve({
+      test: R.always(/\.mdx$/),
+      use: R.append('@mdx-js/loader'),
+    }),
+    R.tap(console.log),
+  )(config.module.rules);
+
+  config.module.rules.push(
+    {
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('awesome-typescript-loader'),
+          // options: {
+          //   errorsAsWarnings: true,
+          // },
+        },
+        // Optional
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+        },
+      ],
+    },
+    mdxRule,
+  );
+
+  config.resolve.extensions.push('.ts', '.tsx');
+
+  fs.writeFileSync(
+    '.storybook/webpack.config.debug.json',
+    JSON.stringify(
+      config,
+      (key, value) => {
+        if (value instanceof RegExp) {
+          return `${value}`;
+        }
+
+        if (value === null) {
+          return null;
+        }
+
+        if (['number', 'string', 'boolean', 'object'].includes(typeof value)) {
+          return value;
+        }
+
+        if (typeof value === 'function') {
+          return '(Function)';
+        }
+
+        return undefined;
       },
-      // Optional
-      {
-        loader: require.resolve("react-docgen-typescript-loader")
-      }
-    ]
-  });
-  config.resolve.extensions.push(".ts", ".tsx");
+      2,
+    ),
+  );
+
   return config;
 };
---
+++ src/start/MDXExample.stories.tsx
@@ -0,0 +1,6 @@
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+
+import MDXExample from './MDXExample.mdx';
+
+storiesOf('MDX Example', module).add('MDX!', () => <MDXExample />);
