e5f5e50cb006a4b5905b1a684b3b0dc5c44a4fbc
Don Hsieh <don.shieh@gmail.com>
2019-06-16 01:39:33 +0800

    feat: Add live-editable code block example and implementation

--- .storybook/MDXComponents/Code.js
+++ .storybook/MDXComponents/Code.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { css } from 'emotion';
+import { css, cx } from 'emotion';
+import { Paper, Typography } from '@material-ui/core';
 import Highlight, { defaultProps } from 'prism-react-renderer';
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 import prismTheme from 'prism-react-renderer/themes/vsDarkPlus';
 
 const cls = {
@@ -10,14 +12,82 @@ const cls = {
     background-color: rgba(179, 157, 219, 0.2);
     padding: 2px 4px;
   `,
+
+  LiveArea: css`
+    margin: 24px 0;
+    padding: 20px 8px;
+    border-radius: 4px;
+    background: #eee;
+  `,
+
+  Label: css`
+    && {
+      color: white;
+      border-radius: 4px 4px 0 0;
+      background: gray;
+      line-height: 1.2;
+      font-size: 0.7em;
+      padding: 6px 12px 4px;
+      margin-bottom: 0;
+      user-select: none;
+      width: fit-content;
+    }
+  `,
+
+  PreviewDividor: css`
+    height: 2px;
+    background: rgb(255, 143, 0);
+  `,
+
+  LivePreview: css`
+    background: white;
+    padding: 20px 0;
+    border-radius: 0 4px 4px 4px;
+    position: relative;
+  `,
+};
+
+const preStyle = {
+  lineHeight: 1.4,
+  fontFamily: "'Ubuntu Mono', monospaces",
+  padding: 20,
 };
 
 export const InlineCode = props => (
   <code className={cls.InlineCode} {...props} />
 );
 
-export const CodeBlock = ({ className, children, ...restProps }) => {
+export const CodeBlock = ({ className, children, live, ...restProps }) => {
   const language = className.replace(/language-/, '');
+
+  if (live) {
+    return (
+      <Paper className={cls.LiveArea} elevation={6}>
+        <LiveProvider code={children} theme={prismTheme} noInline>
+          <Typography
+            className={cls.Label}
+            style={{ background: 'rgb(179, 157, 219)' }}
+          >
+            EDITOR
+          </Typography>
+          <LiveEditor
+            style={{ ...preStyle, padding: 0 }}
+            padding={preStyle.padding}
+          />
+
+          <Typography
+            className={cls.Label}
+            style={{ marginTop: 20, background: 'rgb(255, 143, 0)' }}
+          >
+            PREVIEW
+          </Typography>
+          <div className={cls.PreviewDividor} />
+          <LivePreview className={cls.LivePreview} />
+          <LiveError style={{ color: 'red' }} />
+        </LiveProvider>
+      </Paper>
+    );
+  }
 
   return (
     <Highlight
@@ -27,15 +97,7 @@ export const CodeBlock = ({ className, children, ...restProps }) => {
       language={language}
     >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre
-          className={className}
-          style={{
-            ...style,
-            padding: '20px',
-            lineHeight: 1.4,
-            fontFamily: "'Ubuntu Mono', monospaces",
-          }}
-        >
+        <pre className={className} style={{ ...style, ...preStyle }}>
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({ line, key: i })}>
               {line.map((token, key) => (
--- src/start/MDXExample.stories.tsx
+++ src/start/MDXExample.stories.tsx
@@ -4,8 +4,10 @@ import * as React from 'react';
 import MDXCodeBlocks from './MDXCodeBlocks.mdx';
 import MDXComponents from './MDXComponents.mdx';
 import MDXExample from './MDXExample.mdx';
+import MDXLiveCodeEditor from './MDXLiveCodeEditor.mdx';
 
 storiesOf('MDX Example', module)
   .add('MDX!', () => <MDXExample />)
   .add('MDX Components', () => <MDXComponents />)
-  .add('MDX Code Blocks', () => <MDXCodeBlocks />);
+  .add('MDX Code Blocks', () => <MDXCodeBlocks />)
+  .add('MDX Live Code Editor', () => <MDXLiveCodeEditor />);
