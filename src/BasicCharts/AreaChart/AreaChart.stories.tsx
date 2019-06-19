b21b822715d418577301c920512599a9fa265d04
Don Hsieh <don.shieh@gmail.com>
2019-06-19 14:41:43 +0800

    feat: Add Area chart

---
+++ src/BasicCharts/AreaChart/AreaChart.stories.tsx
@@ -0,0 +1,6 @@
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+
+import AreaChart from './AreaChart.mdx';
+
+storiesOf('Basic Charts', module).add('Area Chart', () => <AreaChart />);
--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,4 +1,12 @@
+/* tslint:disable no-namespace */
+
+/// <reference types="d3" />
+
 declare module '*.mdx' {
   let MDXComponent: (props: any) => JSX.Element;
   export default MDXComponent;
+}
+
+declare namespace d3 {
+  export let autoType: any;
 }
