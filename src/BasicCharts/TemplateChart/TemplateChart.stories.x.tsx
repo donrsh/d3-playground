cd59ec44c9d664892a41004a3b060e337d9dcaff
Don Hsieh <don.shieh@gmail.com>
2019-06-21 14:16:15 +0800

    feat: Add Template Chart

---
+++ src/BasicCharts/TemplateChart/TemplateChart.stories.x.tsx
@@ -0,0 +1,8 @@
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+
+import TemplateChart from './TemplateChart.mdx';
+
+storiesOf('Basic Charts', module).add('Template Chart', () => (
+  <TemplateChart />
+));
